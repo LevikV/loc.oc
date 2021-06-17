99125cd4f4dbfe8030df55ad50734d410ed2051d
LevikV <alex@kplus79.ru>
2021-06-17 20:19:00 +1000

    Получил полный массив категорий

--- catalog/controller/extension/module/tree_cats.php
+++ catalog/controller/extension/module/tree_cats.php
@@ -1,9 +1,37 @@
 <?php
 class ControllerExtensionModuleTreeCats extends Controller {
-	public function index() {
+
+    protected function debug($data) {
+        echo '<pre>' . print_r($data, 1) . '</pre>';
+    }
+
+    public function index() {
 		$this->load->language('extension/module/tree_cats');
 
 		$data['my_title'] = $this->language->get('heading_title');
+        //Получаем ID категории
+        if (isset($this->request->get['path'])) {
+            $parts = explode('_', (string)$this->request->get['path']);
+        } else {
+            $parts = array();
+        }
+        //Получаем ID активной категории
+        if (!empty($parts)) {
+            $data['active'] = end($parts);
+        } else {
+            $data['active'] = 0;
+        }
+        //Подключаем модель
+        $this->load->model('catalog/tree_cats');
+
+        $data['categories'] = array();
+
+        $categories = $this->model_catalog_tree_cats->getTreeCats();
+
+
+        $this->debug($categories);
+
+
         return $this->load->view('extension/module/tree_cats', $data);
 	}
 }
